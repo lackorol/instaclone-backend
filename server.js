c2aace743810a139f12c8d6b8cafd623c77849a8
lackorol <57800924+lackorol@users.noreply.github.com>
2021-06-08 20:08:51 +0200

    recap protected resolvers

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 import { ApolloServer } from "apollo-server";
 import dotenv from 'dotenv';
 import schema from "./schema.js";
+import { getUser, protectResolver } from "./users/users.utils.js";
 
 dotenv.config()
 
 const server = new ApolloServer({
-  schema
+  schema,
+  context : async ({req}) => {
+    return {
+      loggedInUser : await getUser(req.headers.token),
+      protectResolver
+    }
+  }  
 });
 
 const PORT = process.env.PORT
@@ -13,3 +20,13 @@ const PORT = process.env.PORT
 server
   .listen(PORT)
   .then(() => console.log(`🎉 Server is running on http://localhost:${PORT}/ `));
+
+const x = (resolver) => (root, args, context, info) => {
+  if (!context.loggedInUser) {
+    return {
+      ok: false,
+      error: "log in pls"
+    }
+  }
+  return resolver(root, args, context, info);
+}
