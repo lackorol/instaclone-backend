d975c8b86ab6771e33a70a23ce89346cf39629b4
lackorol <57800924+lackorol@users.noreply.github.com>
2021-07-10 22:46:37 +0200

    dotenv fix oneliner at the top of server

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
+require("dotenv").config();
 import { ApolloServer } from "apollo-server-express";
-import dotenv from "dotenv";
 import express from "express";
 import logger from "morgan";
 import { resolvers, typeDefs } from "./schema.js";
 import { getUser, protectResolver } from "./users/users.utils.js";
 
-dotenv.config();
 const PORT = process.env.PORT;
 const server = new ApolloServer({
   resolvers,
   typeDefs,
   introspection: true,
-  playground: true, 
+  playground: true,
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
